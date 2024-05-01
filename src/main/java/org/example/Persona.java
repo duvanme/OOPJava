79b7c27a70cb644f348135b3a3f5c48d849576ce
Duvan Mendoza <duvanpez12@hotmail.com>
2024-05-01 18:04:06 -0500

    Encapsulamiento - Implementación de Clase Persona por Duvan

---
+++ src/main/java/org/example/Persona.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class Persona {
+
+    public String Name;
+    public int Age;
+    public LocalDate Date;
+
+    public Persona(String name, int age, LocalDate date) {
+        Name = name;
+        Age = age;
+        Date = date;
+    }
+
+}
+
