dd8cecddb01a8c8c0bbc71f75bf8260da9d05d21
Duvan Mendoza <duvanpez12@hotmail.com>
2024-05-01 18:54:38 -0500

    Encapsulamiento - Implementación de Validaciones por Duvan

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,8 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        //Persona programador = new Persona("Duvan", 28, LocalDate.of(1996,02,10));
-        //System.out.println("Mi edad es "+programador.Date);
+        Persona programador = new Persona("Duvan", 28, LocalDate.of(1996,02,10));
+        programador.setAge(2);
     }
 }
 
