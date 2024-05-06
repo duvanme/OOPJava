29613f0dafb0c769f5328087534e195471db82fd
Duvan Mendoza <duvanpez12@hotmail.com>
2024-05-06 17:13:42 -0500

    Herencia - Implementación de Constructores por Duvan

--- src/main/java/org/example/Estudiante.java
+++ src/main/java/org/example/Estudiante.java
@@ -1,7 +1,14 @@
 package org.example;
 
-public class Estudiante {
-    private String grado;
+import java.time.LocalDate;
 
+public class Estudiante extends Persona{
+    private String Grado;
 
+    public Estudiante(String grado, String name, int age, LocalDate dateofbirth){
+        super(name, age, dateofbirth);
+
+        Grado = grado;
+
+    }
 }
