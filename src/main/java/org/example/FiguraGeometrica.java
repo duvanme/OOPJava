b4cd45b01a572c2fa5ae34a3b00ee9d52d839d38
Duvan Mendoza <duvanpez12@hotmail.com>
2024-05-06 19:37:48 -0500

    Encapsulamiento y Herencia - Implementación de Métodos para Manipulación de Propiedades por Duvan

--- src/main/java/org/example/FiguraGeometrica.java
+++ src/main/java/org/example/FiguraGeometrica.java
@@ -9,6 +9,20 @@ public class FiguraGeometrica {
         this.color = color;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 
 }
