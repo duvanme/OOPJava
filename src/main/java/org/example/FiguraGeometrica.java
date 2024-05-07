92aeb1cb2740253001c31cf6513436289cae1a41
Duvan Mendoza <duvanpez12@hotmail.com>
2024-05-06 19:34:38 -0500

    Encapsulamiento y Herencia - Implementación de Clase FiguraGeometrica por Duvan

---
+++ src/main/java/org/example/FiguraGeometrica.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class FiguraGeometrica {
+    private String nombre;
+    private String color;
+
+    public FiguraGeometrica(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+
+
+}
